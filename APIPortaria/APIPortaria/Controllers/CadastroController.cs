12dd0c953741422b12b87f49954825ab4a815e3e
Enzo Rodrigues Oliveira <68085261+Enzo-R@users.noreply.github.com>
2022-03-22 16:29:14 -0300

    Add files via upload
    First REST API developed.

---
+++ APIPortaria/APIPortaria/Controllers/CadastroController.cs
@@ -0,0 +1,23 @@
+﻿using APIPortaria.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPortaria.Controllers
+{
+    
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("api/[controller]/AddCadastro")]
+        public async Task<VisitaCadastro> GetCadastro(VisitaCadastro vc)
+        {
+            await vc.AddCadastro();
+            return vc;
+        }
+    }
+}
---
+++ APIPortaria/APIPortaria/Controllers/DestinoController.cs
@@ -0,0 +1,25 @@
+﻿using APIPortaria.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPortaria.Controllers
+{
+
+    [ApiController]
+    public class DestinoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]/GetDestino")]
+        public async Task<List<VisitaDestino>> Get()
+        {
+            List<VisitaDestino> temp = await VisitaDestino.GetVisitaDestino();
+            return temp;
+        }
+      
+
+
+    }
+}
